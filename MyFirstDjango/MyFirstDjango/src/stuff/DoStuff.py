8e4cb31c11a74cfe4f5733dd7414161bf9adc4b7
Mirco Kraenz <prosingularity@yahoo.de>
2014-08-29 18:09:54 +0200

    testing

---
+++ MyFirstDjango/MyFirstDjango/src/stuff/DoStuff.py
@@ -0,0 +1,17 @@
+'''
+Created on 29.08.2014
+
+@author: proSingularity
+'''
+
+class MyClass(object):
+    '''
+    classdocs
+    '''
+
+
+    def __init__(self, params):
+        '''
+        Constructor
+        '''
+        
