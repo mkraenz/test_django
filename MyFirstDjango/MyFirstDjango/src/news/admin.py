f865376989839650e2912cb03922a49d6d6d57f2
Mirco Kraenz <prosingularity@yahoo.de>
2014-08-30 22:01:50 +0200

    first view, proper admin page

--- MyFirstDjango/MyFirstDjango/src/news/admin.py
+++ MyFirstDjango/MyFirstDjango/src/news/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
 
 # Register your models here.
+
+from news.models import Meldung, Kommentar
+
+class PageAdmin( admin.ModelAdmin ):
+    pass
+
+
+admin.site.register( Meldung, PageAdmin )
+admin.site.register( Kommentar, PageAdmin)
--- MyFirstDjango/MyFirstDjango/src/news/views.py
+++ MyFirstDjango/MyFirstDjango/src/news/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+from news.models import Meldung, Kommentar 
+from django.http import HttpResponse 
+ 
+def meldungen(request): 
+    zeilen = [] 
+    for m in Meldung.objects.all(): 
+        zeilen.append("Titel: '%s' vom %s" % (m.title, 
+                                              m.timestamp)) 
+        zeilen.append("Text: %s" % m.text) 
+        zeilen.append("") 
+        zeilen.append("-" * 30) 
+        zeilen.append("") 
+ 
+    antwort = HttpResponse("\n".join(zeilen)) 
+    antwort["Content-Type"] = "text/plain" 
+    return antwort
